1575972a3abda23339c3c4eb1c8d6daedd76efc9
elmerNoob <kikoypalz1221@gmail.com>
2021-05-07 10:10:31 +0800

    Initial Version

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+const port = 3000;
+
+
+
+// app.get('/', function(req, res){
+//     // window.location.reload(true);
+
+
+
+
+// });
+
+app.all('/',function(req, res, next){
+    let dictAPI = 'https://api.dictionaryapi.dev/api/v2/entries/en_US/';
+    let word = req.body.word;
+    let url = dictAPI + word;
+    // let wordJSON
+
+    request(url, function(req, response){
+        let wordJSON = JSON.parse(response.body);
+        // JSONvar = wordJSON;
+
+        res.render('home.ejs',{dict: wordJSON});
+    });
+    // res.render('home.ejs', { dict: wordJSON});
+
+
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port port!`));
